18731d99bcdf9eb7755a282992812075f0449ac2
Marjan Stojanovski <macikb8@yahoo.com>
2023-09-06 19:58:30 +0200

    create shoecard component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import { shoes, statistics } from "../constants";
-import { Button, ShoeCard } from "../components";
+import Button from "../components/Button";
 import { bigShoe1 } from "../assets/images";
 import { arrowRight } from "../assets/icons";
 
@@ -56,12 +56,12 @@ const Hero = () => {
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((image, index) => (
             <div key={index}>
-              <ShoeCard
+              {/* <ShoeCard
                 index={index}
                 imgURL={image}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
-              />
+              /> */}
             </div>
           ))}
         </div>
