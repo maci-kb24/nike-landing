ca3db09985db7f9b63e6cd4c09882953fe5f4c85
Marjan Stojanovski <macikb8@yahoo.com>
2023-09-07 21:17:18 +0200

    create specialoffer component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 import { shoes, statistics } from "../constants";
-import Button from "../components/Button";
-import ShoeCard from "../components/ShoeCard";
+import { Button } from "../components";
+import { ShoeCard } from "../components";
 import { bigShoe1 } from "../assets/images";
 import { arrowRight } from "../assets/icons";
 
